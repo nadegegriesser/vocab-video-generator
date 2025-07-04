36bab6394a15a220d88b73e7f97dc74ce005779b
Griesser, Nadege <Nadege.Griesser@vector.com>
2025-07-04 18:56:08 +0200

    wad

--- src/file.ts
+++ src/file.ts
@@ -1,25 +1,24 @@
 import fs from 'fs';
-import path from 'path';
 
-export function loadFile<Type>(path: string): Type[] {
-    if (!fs.existsSync(path)) {
+export function loadFile<Type>(filePath: string): Type[] {
+    if (!fs.existsSync(filePath)) {
         return [];
     }
-    const raw = fs.readFileSync(path, 'utf-8');
+    const raw = fs.readFileSync(filePath, 'utf-8');
     return <Type[]>JSON.parse(raw);
 }
 
-export function generateAudioList(dir: string) {
-  const files = fs.readdirSync(dir)
+export function generateAudioList(dirPath: string) {
+  const files = fs.readdirSync(dirPath)
     .filter(file => file.endsWith('.wav'))
     .sort(); 
 
   if (files.length === 0) {
-    console.error('❌ No .wav files found in', dir);
-    process.exit(1);
+    console.error('❌ No .wav files found in', dirPath);
+    return;
   }
 
   const lines = files.map(file => `file '${file}'`);
-  fs.writeFileSync(`${path}/audio-list.txt`, lines.join('\n'));
-  console.log(`✅ Generated ${path}/audio-list.txt with ${files.length} entries.`);
+  fs.writeFileSync(`${dirPath}/audio-list.txt`, lines.join('\n'));
+  console.log(`✅ Generated ${dirPath}/audio-list.txt with ${files.length} entries.`);
 }
