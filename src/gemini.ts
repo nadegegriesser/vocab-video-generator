900b5162191fb296232f367150009e5111a528d1
Griesser, Nadege <Nadege.Griesser@vector.com>
2025-07-04 15:54:39 +0200

    dh

--- src/gemini.ts
+++ src/gemini.ts
@@ -1,4 +1,4 @@
-import { GoogleGenAI } from '@google/genai';
+import { GoogleGenAI, Type } from '@google/genai';
 import { TopicEntry, VocabEntry } from './types';
 import dotenv from 'dotenv';
 
@@ -24,7 +24,25 @@ Return as JSON array with keys: source, target.
 
     const response = await ai.models.generateContent({
         model: 'gemini-2.5-flash',
-        contents: prompt
+        contents: prompt,
+        config: {
+            responseMimeType: "application/json",
+            responseSchema: {
+                type: Type.ARRAY,
+                items: {
+                    type: Type.OBJECT,
+                    properties: {
+                        source: {
+                            type: Type.STRING
+                        },
+                        target: {
+                            type: Type.STRING
+                        }
+                    },
+                    propertyOrdering: ["source", "target"]
+                }
+            }
+        }
     });
 
     console.log(response.text);
@@ -40,7 +58,7 @@ export async function generateVocabForTopic(
 ): Promise<VocabEntry[]> {
     const prompt = `
 Generate ${count} vocabulary entries for the topic "${topic}" in ${sourceLang} for language learners at ${level} level.
-For nouns add a defined article.
+For nouns add a defined article writter in lowercase. Do not add anything in parenthesis after the word.
 The topics should be relevant for learners of ${sourceLang} translating into ${targetLang}.
 For each word, provide:
 - The word in ${sourceLang}
@@ -52,7 +70,31 @@ Return as JSON array of objects with keys: source, target, exampleSource, exampl
 
     const response = await ai.models.generateContent({
         model: 'gemini-2.5-flash',
-        contents: prompt
+        contents: prompt,
+        config: {
+            responseMimeType: "application/json",
+            responseSchema: {
+                type: Type.ARRAY,
+                items: {
+                    type: Type.OBJECT,
+                    properties: {
+                        source: {
+                            type: Type.STRING
+                        },
+                        target: {
+                            type: Type.STRING
+                        },
+                        exampleSource: {
+                            type: Type.STRING
+                        },
+                        exampleTarget: {
+                            type: Type.STRING
+                        }
+                    },
+                    propertyOrdering: ["source", "target"]
+                }
+            }
+        }
     });
 
     console.log(response.text);
