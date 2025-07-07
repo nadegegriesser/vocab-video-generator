563dc2c0f16b80367ee8dc6919e744c552c9e6a1
Griesser, Nadege <Nadege.Griesser@vector.com>
2025-07-07 18:05:09 +0200

    a

--- scripts/generate-video.ts
+++ scripts/generate-video.ts
@@ -1,4 +1,5 @@
 import { generateAudioList } from '../src/file.js';
+import { ffmpeg } from 'fluent-ffmpeg';
 
 const args = process.argv.slice(2);
 const dir = args[0];
@@ -7,6 +8,10 @@ const dir = args[0];
 (async () => {
     try {
         generateAudioList(dir);
+        ffmpeg('')
+         .videoFilters("drawtext=text='Dein Text':fontcolor=white:fontsize=48:x=(w-text_w)/2:y=(h-text_h)/2")
+         .output('output.mp4')
+         .run();
     } catch (err) {
         console.error('❌ Failed to generate video:', err);
     }
