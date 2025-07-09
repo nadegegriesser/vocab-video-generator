d7a3a767f5fb4eec642c96a95f3c1e0c55c8ecf1
Griesser, Nadege <Nadege.Griesser@vector.com>
2025-07-09 17:58:05 +0200

    gh

--- scripts/generate-video.ts
+++ scripts/generate-video.ts
@@ -1,19 +1,33 @@
-import { generateAudioList } from '../src/file.js';
-import { FfmpegCommand } from 'fluent-ffmpeg';
+import { loadFile } from '../src/file.js';
+import * as ffmpeg from 'fluent-ffmpeg';
+import { VocabEntry } from '../src/types.js';
 
 const args = process.argv.slice(2);
 const dir = args[0];
+const vocabPath = `${dir}/vocab.json`;
 
 
 (async () => {
     try {
-        generateAudioList(dir);
-        new FfmpegCommand()
+        let i = 0;
+        for (const vocab of loadFile<VocabEntry>(vocabPath)) {
+            console.log(vocab);
+            //generateAudioList(dir);
+            const index = String(i).padStart(2, '0');
+            new ffmpeg.FfmpegCommand()
+                .input('data/image.jpg')
+                .input(`audio/${index}.wav`)
+                .videoFilters(`drawtext=text='${vocab.source}':fontcolor=white:fontsize=48:x=(w-text_w)/2:y=(h-text_h)/2`)
+                .output('output.mp4')
+                .run();
+                return;
+        }
+        /* new FfmpegCommand()
             .input('data/image.jpg')
-            .input('${dir}/image.jpg')
+            .input('combined.wav')
             .videoFilters("drawtext=text='Dein Text':fontcolor=white:fontsize=48:x=(w-text_w)/2:y=(h-text_h)/2")
             .output('output.mp4')
-            .run();
+            .run(); */
     } catch (err) {
         console.error('❌ Failed to generate video:', err);
     }
