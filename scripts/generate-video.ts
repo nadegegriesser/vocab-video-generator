0d310308afaa9ed2a1fadae1987a0fa21876555b
Griesser, Nadege <Nadege.Griesser@vector.com>
2025-07-10 18:05:01 +0200

    5z

--- scripts/generate-video.ts
+++ scripts/generate-video.ts
@@ -1,7 +1,6 @@
-import fs from 'fs';
 import { loadFile } from '../src/file.js';
-import FfmpegCommand from 'fluent-ffmpeg';
 import { VocabEntry } from '../src/types.js';
+import { exec } from 'child_process';
 
 const args = process.argv.slice(2);
 const dir = args[0];
@@ -15,26 +14,15 @@ const vocabPath = `${dir}/vocab.json`;
             console.log(vocab);
             //generateAudioList(dir);
             const index = String(i).padStart(2, '0');
-            FfmpegCommand('data/image.jpg')
-                .input(`${dir}/audio/${index}.wav`)
-                .loop()
-                /*.videoFilters([{
-                    filter: 'drawtext',
-                    options: {
-                        text: vocab.source,
-                        fontcolor: 'red',
-                        fontsize: 32,
-                        x: '(w-text_w)/2',
-                        y: '(h-text_h)/2'
-                    }
-                }])*/
-                .videoCodec('libx264')
-                .audioCodec('aac')
-                .audioBitrate('192k')
-                .outputOption('-pix_fmt yuv420p')
-                .on('start', (cmdline) => console.log('>>>> CMD', cmdline))
-                .on('error', (cmdline) => console.log('>>>> CMD', cmdline))
-                .save(`${dir}/audio/${index}.mp4`);
+            const command = `ffmpeg -y -loop 1 -i data/image.jpg -i ${dir}/audio/${index}.wav -vf \"drawtext=text='${vocab.source}':fontcolor=white:fontsize=32:x=(w-text_w)/2:y=(h-text_h)/2\" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -shortest -pix_fmt yuv420p ${dir}/audio/${index}.mp4`
+            exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+                console.error(`stderr: ${stderr}`);
+            });
             return;
 
             /*ffmpeg -y -loop 1 -i ../../../../image.jpg -i combined.wav \
