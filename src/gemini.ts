53d4ad7917c22c5e5f0ac131241c99bea79343a8
Griesser, Nadege <Nadege.Griesser@vector.com>
2025-07-14 18:55:14 +0200

    f

--- src/gemini.ts
+++ src/gemini.ts
@@ -230,10 +230,7 @@ export async function synthesizeSpeech(
             },
         },
     };
-    let text = `STYLE DESCRIPTION:
-${name1}: speaks ${sourceLang} language. She is ${style1}.
-${name2}: speaks ${targetLang} language. He is ${style2}.
-SCRIPT:`;
+    let text = `TTS the following conversation between ${name1}, who speaks ${sourceLang} language - she is ${style1} - and ${name2}, who speaks ${targetLang} language - he is ${style2}.`;
     let i = 0;
     for (const v of vocab) {
         text += `
